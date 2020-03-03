0147f65e1053bf3989431543bc3dfa6ccd69b5e8
rafalinliyosar <rafalin.liyosar@gmail.com>
2020-03-03 11:40:30 +0530

    count component

---
+++ src/CountComponent/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default class CountComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0
+        };
+    }
+    incrementClick = () => {
+        this.setState({
+            count: this.state.count + 1
+        });
+    }
+    render() {
+        return (<div>Hello world, This is count component: {this.state.count}
+            <button onClick={this.incrementClick}>Increment</button>
+        </div>);
+    }
+}
